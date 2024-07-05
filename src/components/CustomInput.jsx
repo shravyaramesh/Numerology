99273910b61a513ae619e9a978f8a3d452804a46
Shravya Ramesh <shravyaramesh12@gmail.com>
2024-07-05 19:27:57 -0400

    Created custom input component

---
+++ src/components/CustomInput.jsx
@@ -0,0 +1,11 @@
+import '../styles/mainpage.css';
+
+
+export default function CustomInput ({label}) {
+    
+
+    return <div className="detail-item-container">
+        <label>{label}</label>
+        <input />
+    </div>
+}
